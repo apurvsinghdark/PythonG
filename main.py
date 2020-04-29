63b858ac1b2cc3dc39a1231d4be811a2bc6091fc
Apurv singh <apurvsingh9713@gmail.com>
2020-04-29 14:00:00 +0530

    card class deck class

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
@@ -5,3 +6,44 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
          'Queen':10, 'King':10, 'Ace':11}
 
 playing = True
+
+#Card Class
+
+class Card:
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.rank +  ' of '+ self.suit
+
+
+class Deck:
+
+    def __init__(self):
+        self.deck = [] #empty List
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank)) #build Card objects and them to the list
+
+    def __str__(self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n' +card.__str__() # collect collection of card from __init__ function
+        return 'The deck has: ' + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+
+
+
+
+
+if __name__ == '__main__':
+
+    test_deck = Deck()
+    print(test_deck)
